3c0738d595c429437c1f4fad96d389d41b31cdeb
Shihao Shen <shihao@shihaoshen.qzz.io>
2026-03-14 07:42:01 +0800

    Update auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "ShihaoShenDev/Scoop-Bucket:main"
+    [String]$upstream = "ShihaoShenDev/ScoopBucket:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
